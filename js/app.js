ef4bad3ca194e370c2c652c53135b7a907fab3cb
Matias Ricardo Carrillo Sanchez <matiascarrillo115@gmail.com>
2024-06-25 02:54:14 -0500

    Clase-7 + un poco de experimentacion

--- js/app.js
+++ js/app.js
@@ -1,73 +1,48 @@
 'use strict';
-
-const Seattle={
-    name:'Seattle',
-    address:`2901 3rd avenue #300, Seattle`,
-    contactInfo:'123-456-789',
-    workingHours:`6a.m.-7p.m.`,
-    cookiesPerHour:[],
-    minCustomerPerHour:23,
-    maxCustomerPerHour:65,
-    avgCookiesPerSale:6.3,
-    estimate: function(){
-        this.cookiesPerHour=estimateSales(this)
-    },
+Location.prototype.estimate = function(){
+    this.cookiesPerHour=estimateSales(this)
 };
-const Tokyo={
-    name:'Tokyo',
-    address:`1 Chome 1-2 Oshiage, Sumida City, Tokyo 121-8634`,
-    contactInfo:'222-222-2222',
-    workingHours:`6a.m.-7p.m.`,
-    cookiesPerHour:[],
-    minCustomerPerHour:3,
-    maxCustomerPerHour:24,
-    avgCookiesPerSale:4.6,
-    estimate: function(){
-        this.cookiesPerHour=estimateSales(this)
-    },
-}
-const Dubai={
-    name:'Dubai',
-    address:`1 Sheikh Mohammed bin Rashid Blvd - Dubai`,
-    contactInfo:'333-333-3333',
-    workingHours:`6a.m.-7p.m.`,
-    cookiesPerHour:[],
-    minCustomerPerHour:11,
-    maxCustomerPerHour:38,
-    avgCookiesPerSale:3.7,
-    estimate: function(){
-        this.cookiesPerHour=estimateSales(this)
-    },
-}
-const Paris={
-    name:'Paris',
-    address:`Champ de Mars, 5 Avenue Antole France,75007 Paris`,
-    contactInfo:'444-444-4444',
-    workingHours:`6a.m.-7p.m.`,
-    cookiesPerHour:[],
-    minCustomerPerHour:20,
-    maxCustomerPerHour:38,
-    avgCookiesPerSale:2.3,
-    estimate: function(){
-        this.cookiesPerHour=estimateSales(this)
-    },
-}
-const Lima={
-    name:'Lima',
-    address:`Ca.Gral. Borgoño cuadra 8, Miraflores 15074`,
-    contactInfo:'555-555-5555',
-    workingHours:`6a.m.-7p.m.`,
-    cookiesPerHour:[],
-    minCustomerPerHour:20,
-    maxCustomerPerHour:38,
-    avgCookiesPerSale:2.3,
-    estimate: function(){
-        this.cookiesPerHour=estimateSales(this)
-    },
-}
+function Location(name, address, contactInfo, workingHours, cookiesPerHour, minCustomerPerHour, maxCustomerPerHour, avgCookiesPerSale){
+    this.name = name;
+    this.address = address;
+    this.contactInfo = contactInfo;
+    this.workingHours = workingHours;
+    this.cookiesPerHour = cookiesPerHour;
+    this.minCustomerPerHour = minCustomerPerHour;
+    this.maxCustomerPerHour = maxCustomerPerHour;
+    this.avgCookiesPerSale = avgCookiesPerSale;
+};
+
+const Seattle = new Location(`Seattle`, `2901 3rd avenue #300, Seattle`,'123-456-789',`6a.m.-7p.m.`, [], 23, 65,6.3);
+const Tokyo = new Location(`Tokyo`,`1 Chome 1-2 Oshiage, Sumida City, Tokyo 121-8634`,`222-222-2222`,`6a.m.-7p.m.`,[],3,24,4.6);
+const Dubai = new Location(`Dubai`,`1 Sheikh Mohammed bin Rashid Blvd - Dubai`,`333-333-3333`,`6a.m.-7p.m.`,[],11,38,3.7);
+const Paris = new Location(`Paris`,`Champ de Mars, 5 Avenue Antole France,75007 Paris`,'444-444-4444',`6a.m.-7p.m.`,[],20,38,2.3);
+const Lima = new Location(`Lima,`,`Ca.Gral. Borgoño cuadra 8, Miraflores 15074`,`555-555-5555`,`6a.m.-7p.m.`,[],20,38,2.3);
+
 const hours =[`6am`,`7am`,`8am`,`9am`,`10am`,`11am`,`12pm`,`1pm`,`2pm`,`3pm`,`4pm`,`5pm`,`6pm`,`7pm`];
 const stores =[Seattle,Tokyo,Dubai,Paris,Lima];
 
+function runApplication(){
+    for(let i=0;i<stores.length;i++){
+        stores[i].estimate();
+        renderSales(stores[i]);
+    }
+};
+function runIndex(){
+    for(let i=0;i<stores.length;i++){
+        renderIndex(stores[i]);
+    }
+};
+function columnFiller(columnText){
+    const column = document.createElement("th");
+    column.textContent= columnText
+    return column;
+};
+function rowFiller(rowText){
+    const row = document.createElement("tr")
+    row.textContent= rowText
+    return row;
+}
 function generarNumeroRandom(min, max) {
     return Math.round(Math.random() * (max - min) + min);
 };
@@ -82,26 +57,38 @@ function estimateSales(store){
 };
 function renderSales(store){
     let total=0;
-    const root=document.getElementById('root');
-    const location=document.createElement('section');
-    location.classList.add('location');
-    root.appendChild(location);
-    const title=document.createElement('h2');
-    title.textContent=store.name;
-    location.appendChild(title);
-    const list=document.createElement('ul')
-    location.appendChild(list)
     for(let i =0;i<hours.length;i++){
         total += store.cookiesPerHour[i];
-        const listItem=document.createElement('li');
-        listItem.textContent=`${hours[i]}: ${store.cookiesPerHour[i]} cookies`;
-        list.appendChild(listItem);
-    }
-    const totalItems=document.createElement('li');
-    totalItems.textContent=`total ${total} cookies`;
-    list.appendChild(totalItems);
-}
+        return total
+    };
+};
+function renderTable(){
+    runApplication();
+    const salesTable = document.getElementById("sales-table");
+    const table = document.createElement("table");
+    salesTable.appendChild(table)
+    const thead = document.createElement("thead");
+    table.appendChild(thead);
+    const columns = document.createElement("tr");
+    thead.appendChild(columns);
+    const empty =document.createElement("th");;
+    columns.appendChild(empty);
+    for (let i = 0; i < hours.length; i++){
+        const th =columnFiller(hours[i]);
+        columns.appendChild(th);
+    };
+    for(let i=0;i<stores.length;i++){
+        stores[i].name;
+        const row = rowFiller(stores[i].name);
+        thead.appendChild(row);
+    };
+    // for(let i=0;i<stores.length;i++){
+    // const th = columnFiller(stores[i].cookiesPerHour);
+    // .appendChild(th)
+    // }
+};
 function renderIndex(store){
+
     const fill=document.getElementById('fill');
     const location=document.createElement('section');
     location.classList.add('location');
@@ -119,14 +106,4 @@ function renderIndex(store){
     addressInfo.textContent= `Location: ${store.address}`;
     location.appendChild(addressInfo);
 };
-function runApplication(store){
-    for(let i=0;i<stores.length;i++){
-        stores[i].estimate();
-        renderSales(stores[i]);
-    }
-};
-function runIndex(store){
-    for(let i=0;i<stores.length;i++){
-        renderIndex(stores[i]);
-    }
-};
+
